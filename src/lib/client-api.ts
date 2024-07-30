ff101df9f5880fbabeecdca5014e4fb3e17d57a1
Bartu OZEL <info@bartuozel.com>
2024-07-30 04:23:33 +0300

    update api url

--- src/lib/client-api.ts
+++ src/lib/client-api.ts
@@ -4,6 +4,6 @@ export const clientApi = new Api({
   baseURL:
     process.env.NODE_ENV === "development"
       ? "http://localhost:3001"
-      : "https://magnificent-patience-production.up.railway.app/",
+      : "https://trendmall-api.up.railway.app/",
   withCredentials: true,
 })
